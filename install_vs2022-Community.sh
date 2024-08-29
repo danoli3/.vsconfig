055342c2835101ec8b6604051b77e11257a35d70
Dan Rosser <danoli3@gmail.com>
2024-08-30 00:13:21 +1000

    VS2022 install via VS Installer with

---
+++ install_vs2022-Community.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+"C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" modify --installPath "C:\Program Files\Microsoft Visual Studio\2022\Community" --config ".vsconfig" --passive --allowUnsignedExtensions
---
+++ install_vs2022-Professional.sh
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" modify --installPath "C:\Program Files\Microsoft Visual Studio\2022\Professional" --config ".vsconfig" --passive --allowUnsignedExtensions
